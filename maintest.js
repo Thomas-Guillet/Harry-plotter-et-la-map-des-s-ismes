5ddb4837e336cb16103d00bf2f351697985ed96e
Unknown <thomas.guillet35@hotmail.fr>
2018-04-08 21:41:47 +0200

    back to normal
    rien ne fonctionne ! machine arrière toute mon commandant !

--- maintest.js
+++ maintest.js
@@ -17,7 +17,8 @@ let maxY = 0;
 var items = [];
 ctx.lineWidth = 1;
 svgExportCtx.lineWidth = 1;
-const url = 'http://seismes.cleverapps.io/api/2017-07-08/2017-08-09/5';
+const url = 'http://seismes.cleverapps.io/api/2016-04-01/2016-05-01/5';
+// const url = 'http://seismes.cleverapps.io/api/2017-07-08/2017-08-09/5';
 // const url = 'http://seismes.cleverapps.io/api/2017-01-08/2017-01-18/0';
 
 const normalPDF = (x, mu, sigma) => {
@@ -32,61 +33,49 @@ let c = 0;
 
 
 const drawline = (y) => {
-// 	ctx.fillStyle = "rgba(255, 0, "+c+", 1)";
-// ctx.strokeStyle = 'rgba(255, 0, '+c+', 1)';
 	ctx.strokeStyle = 'black';
 	svgExportCtx.strokeStyle = 'black';
-	let x = 10;
-	
+	let x = 0;
+
 	ctx.beginPath();
 	svgExportCtx.beginPath();
 	ctx.moveTo(0, y);
 	svgExportCtx.moveTo(0, y);
 	let newY = 0;
-	let stateLine = true;
-	let lastSwitch = -50;
+	// let stateLine = true;
+	// let lastSwitch = -50;
 	for (let i = 0; i < (points + 1); i++) {
-		x += dx;
 		newY = 0;
-        for (let v = 0; v < items.length; v++) {
+    for (let v = 0; v < items.length; v++) {
 			const delta = Math.abs(y - items[v][1]);
             newY += y - ((1000 * normalPDF(x, items[v][0], 15 + delta))*(items[v][2]*0.5))*items.length;
 		}
 		if(maxY > (newY/items.length)){
 			maxY = newY/items.length;
 		}
-		let pixelData = ctx.getImageData(x,newY/items.length,1,1);
-		const opacity = pixelData.data[3];
-		if(opacity != 0 && stateLine && i - lastSwitch >= 5){
-			ctx.stroke();
-			svgExportCtx.stroke();
-			stateLine = false;
-			lastSwitch = i;
-		}else if(opacity != 0 && !stateLine && i - lastSwitch >= 5){
-			stateLine = true;
-			ctx.beginPath();
-			svgExportCtx.beginPath();
-			lastSwitch = i;
-		}
-		if(stateLine){
+		// let pixelData = ctx.getImageData(x,newY/items.length,1,1);
+		// const opacity = pixelData.data[3];
+		// if(opacity != 0 && stateLine && i - lastSwitch >= 2){
+		// 	ctx.stroke();
+		// 	svgExportCtx.stroke();
+		// 	stateLine = false;
+		// 	lastSwitch = i;
+		// }else if(opacity != 0 && !stateLine && i - lastSwitch >= 2){
+		// 	stateLine = true;
+		// 	ctx.beginPath();
+		// 	svgExportCtx.beginPath();
+		// 	lastSwitch = i;
+		// }
+		// if(stateLine){
 			ctx.lineTo(x, newY/items.length);
 			svgExportCtx.lineTo(x, newY/items.length);
-		}else{
-			ctx.moveTo(x, newY/items.length);
-			svgExportCtx.moveTo(x, newY/items.length);
-		}
-		
-		
+		// }else{
+		// 	ctx.moveTo(x, newY/items.length);
+		// 	svgExportCtx.moveTo(x, newY/items.length);
+		// }
+		x += dx;
 	}
 
-	// ctx.lineTo(x, H);
-	// svgExportCtx.lineTo(x, H);
-	// ctx.lineTo(0, H);
-	// svgExportCtx.lineTo(0, H);
-	// ctx.lineTo(0, y);
-	// svgExportCtx.lineTo(0, y);
-	// ctx.closePath();
-	// svgExportCtx.closePath();
 	ctx.stroke();
 	svgExportCtx.stroke();
 
@@ -117,23 +106,24 @@ fetch(url)
 		drawline(y);
 		console.log('Ligne '+i+' sur '+lignes);
 	}
-	// maxY -= 40;
-	// ctx.lineWidth = 4;
-	// svgExportCtx.lineWidth = 4;
-	// ctx.beginPath();
-	// svgExportCtx.beginPath();
-	// ctx.moveTo(0, maxY);
-	// svgExportCtx.moveTo(0, maxY);
-	// ctx.lineTo(W, maxY);
-	// svgExportCtx.lineTo(W, maxY);
-	// ctx.lineTo(W, H);
-	// svgExportCtx.lineTo(W, H);
-	// ctx.lineTo(0, H);
-	// svgExportCtx.lineTo(0, H);
-	// ctx.closePath();
-	// svgExportCtx.closePath();
-	// ctx.stroke();
-	// svgExportCtx.stroke();
+
+	maxY -= 40;
+  ctx.lineWidth = 4;
+  svgExportCtx.lineWidth = 4;
+  ctx.beginPath();
+  svgExportCtx.beginPath();
+  ctx.moveTo(0, maxY);
+  svgExportCtx.moveTo(0, maxY);
+  ctx.lineTo(W, maxY);
+  svgExportCtx.lineTo(W, maxY);
+  ctx.lineTo(W, H);
+  svgExportCtx.lineTo(W, H);
+  ctx.lineTo(0, H);
+  svgExportCtx.lineTo(0, H);
+  ctx.closePath();
+  svgExportCtx.closePath();
+  ctx.stroke();
+  svgExportCtx.stroke();
 
     console.log('ended !');
     console.log(items);
@@ -144,20 +134,20 @@ const download = (filename, content) => {
 	let pseudoLink = document.createElement("a");
 	pseudoLink.setAttribute("href", "data:image/svg+xml;charset=utf-8," + encodeURIComponent(content));
 	pseudoLink.setAttribute("download", filename);
-  
+
 	pseudoLink.style.display = "none";
 	document.body.appendChild(pseudoLink);
-  
+
 	pseudoLink.click();
-  
+
 	document.body.removeChild(pseudoLink);
   }
-  
+
   document.querySelector('button').addEventListener(
 	"click", () => {
 	  let svg = svgExportCtx.getSerializedSvg();
 	  let filename = "export.svg";
-  
+
 	  download(filename, svg);
 	},
 	false
